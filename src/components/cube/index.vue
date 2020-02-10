33daaceaf340864ee1d1c111c65a4c2fdb35c518
spraylee <spraylee@qq.com>
2020-02-10 20:52:06 +0800

    add 3d box

---
+++ src/components/cube/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="cube auto-rotate">
+    <div class="surface surface-1">1</div>
+    <div class="surface surface-2">2</div>
+    <div class="surface surface-3">3</div>
+    <div class="surface surface-4">4</div>
+    <div class="surface surface-5">5</div>
+    <div class="surface surface-6">6</div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+@keyframes rotate {
+  from {
+    transform: rotate3d(1, 0.5, -1, 0deg);
+  }
+  to {
+    transform: rotate3d(1, 0.5, -1, 360deg);
+  }
+}
+
+.cube {
+  display: block;
+  position: fixed;  
+  left: 50vw;
+  top: 50vh;
+  width: 0;
+  height: 0;
+  transform-style: preserve-3d;
+  border: 10px solid red;
+  &.auto-rotate {
+    animation: rotate 10s;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+  }
+  .surface {
+    position: absolute;
+    left: -100px;
+    top: -100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 48px;
+    width: 200px;
+    height: 200px;
+    box-sizing: border-box;
+    border: 2px solid #333;
+    color: black;
+    background-color: #ccc;
+    opacity: 0.9;
+  }
+  .surface-1 {
+    transform: translateZ(100px);
+    background-color: red;
+  }
+  .surface-2 {
+    transform: translateZ(-100px);
+    background-color: yellow;
+  }
+  .surface-3 {
+    transform: translateX(100px) rotateY(90deg);
+    background-color: blue;
+  }
+  .surface-4 {
+    transform: translateX(-100px) rotateY(90deg);
+    background-color: green;
+  }
+  .surface-5 {
+    transform: translateY(-100px) rotateX(90deg);
+    background-color: #888;
+  }
+  .surface-6 {
+    transform: translateY(100px) rotateX(90deg);
+    background-color: white;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <cube></cube>
   </div>
 </template>
 
@@ -9,11 +10,12 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 import '../core/main.ts'
-
+import cube from '../components/cube/index.vue'
 export default {
   name: 'home',
   components: {
     HelloWorld,
+    cube,
   },
 }
 </script>
